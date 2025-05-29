c8ac1b19a043df62f6b10375a6347cdac3d2c8bc
YasHKaRo <ro517zh@gmail.com>
2025-05-29 21:47:54 +0300

    Fix def find_IP

--- week_1/hostname/main.py
+++ week_1/hostname/main.py
@@ -13,7 +13,8 @@ def find_host():
     hostname = os.open("/etc/hostname", 0)
     return os.read(hostname, 256)
 def find_IP():
-    return print(socket.gethostbyaddr(find_host()[2:8]))
+    hostname = os.open("/etc/hostname", 0)
+    return print(socket.gethostbyaddr(hostname[2:8]))
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     answer()
